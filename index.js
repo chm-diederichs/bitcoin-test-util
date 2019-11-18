82f8940e5a67401b9e35275b00c827d50751ef8f
Christophe Diederichs <chris.d.itunes@gmail.com>
2019-11-18 19:02:31 +0100

    refactored into a single exported class, testing added

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const Client = require('bitcoin-core')
 const assert = require('nanoassert')
 const pMap = require('p-map')
-
-const rpcInfo = {
-  port: 18447,
-  username: 'lnd',
-  password: 'password',
-  network: 'regtest',
-  datadir: './.regtest',
-}
 
 const addressTypes = [
   'bech32',
@@ -16,93 +7,234 @@ const addressTypes = [
   'p2sh-segwit'
 ]
 
-module.exports = generateAndCollect
+module.exports = class TestNode {
+  constructor (node) {
+    this.node = node
 
-const node = new Client(rpcInfo)
+    this. genAddress = null
 
-generateAndCollect(10, 10, 5)
+    this.coinbase = []
+    this.coinbaseAmt = null
 
-async function generateAndCollect (amount, blocks, splitBy = [1], addressType = 'legacy', fees = 0.005) {  
-  // equal split may be specified by given desired number of UTXOs as an int
-  splitBy = typeof splitBy === 'object'
-    ? splitBy
-    : new Array(splitBy).fill(1 / splitBy)
+    this.unspent = []
+    this.regularBase = []
+  }
 
-  // correct for floating point error 
-  const correction = splitBy.reduce((acc, value) => acc + value) - 1
-  splitBy[0] -= correction
+  async init (addressType = 'legacy') {
+    await this.updateUnspent()
+    await this.updateCoinbase()
 
-  // check funds don't exceed available amount
-  assert(splitBy.reduce((acc, value) => acc + value) <= 1, 'total sum cannot exceed available funds')
+    this.genAddress = await this.node.getNewAddress('generate', addressType)
+    this.regularBase = this.unspent.filter(utxo => !this.coinbase.includes(utxo))
+  }
 
-  // generate funds
-  const genAddress = await node.getNewAddress('1', addressType)
-  await node.generateToAddress(blocks, genAddress)
-  
-  // confirm funds
-  await node.generateToAddress(20, genAddress)
+  async updateUnspent () {
+    this.unspent = await this.node.listUnspent()
+  }
 
-  // collect coinbase transactions
-  const coinbaseTxns = await coinbase()
-  
-  // generate rpc inputs to create transaction
-  const selectedInputs = selectTxInputs(coinbaseTxns, amount)
-  // const txOutputs = buildTxOutputs(transferAmounts, amount, transferAddresses)
+  async generate (blocks) {
+    await this.node.generateToAddress(blocks, this.genAddress)
+  }
 
-  // generate transaction outputs
-  const transferAmounts = splitBy.map(portion => amount * portion)
+  async send (inputs, outputs, replaceable = true, locktime = null) {
+    assert(typeof replaceable === 'boolean', 'replaceable flag must be a boolean')
 
-  const txOutputs = await pMap(
-    transferAmounts,
-    createOutput('random'),
-    { concurrency: 5 }
-  )
+    const rawTx = await this.node.createRawTransaction(inputs, outputs, locktime, replaceable)
+    const signedTx = await this.node.signRawTransactionWithWallet(rawTx)
+    const txid = await this.node.sendRawTransaction(signedTx.hex)
 
-  const changeAddress = await node.getNewAddress('change', addressType)
-  const rpcInput = rpcFormat(selectedInputs, txOutputs, changeAddress)
+    return txid
+  }
 
-  // create, sign and send tx
-  const rawTx = await node.createRawTransaction(...rpcInput, null, true)
-  const signedTx = await node.signRawTransactionWithWallet(rawTx)
-  const txid = await node.sendRawTransaction(signedTx.hex)
+  async confirm () {
+    await this.generate(6)
+  }
 
-  // create rbf tx using inputs from first tx
-  const [rbfInputs, rbfOutputs] = [[], []]
+  async sendAndConfirm (inputs, outputs, locktime = null) {
+    const txid = await this.send(inputs, outputs, locktime, false)
 
-  rbfInputs.push(selectedInputs.slice().sort((a, b) => a.amount - b.amount).pop())
-  rbfOutputs.push(txOutputs.slice().sort((a, b) => Object.values(a)[0] - Object.values(b)[0])[0])
+    await this.generate(6)
+    return txid
+  }
 
-  // make rpc input for rbf tx
-  const rbfInput = rpcFormat(rbfInputs, rbfOutputs, changeAddress, 0.0005)
+  // update coinbaseTxns and return available coinbase funds
+  async updateCoinbase () {
+    const currentCoinbase = this.coinbase || []
 
-  // create, sign and send rbf tx
-  const rbfRawTx = await node.createRawTransaction(...rbfInput, null, true)
-  const rbfSignedTx = await node.signRawTransactionWithWallet(rbfRawTx)
-  const rbfTxid = await node.sendRawTransaction(rbfSignedTx.hex)
+    // fetch new utxos
+    await this.updateUnspent()
 
-  // confirm transaction
-  await node.generateToAddress(6, genAddress)
+    // filter already stored coinbase txns
+    const newCoinbaseTxns = []
+    const newUnspent = this.unspent.filter(utxo => !this.coinbase.includes(utxo))
 
-  // list utxos from original tx (should be empty array) and rbf tx
-  node.listUnspent().then(unspent => console.log(unspent.filter(utxo => utxo.txid == txid)))
-  node.listUnspent().then(unspent => console.log(unspent.filter(utxo => utxo.txid == rbfTxid)))
+    await pMap(newUnspent, async utxo => {
+      const txInfo = await this.node.getTransaction(utxo.txid)
+ 
+      // utxo.generated: true iff utxo is coinbase
+      if (txInfo.generated) newCoinbaseTxns.push(utxo)
+    }, { concurrency: 5 })
+
+    // update coinbase array and total coinbase amount
+    this.coinbase = currentCoinbase.concat(newCoinbaseTxns)
+    this.coinbaseAmt = this.coinbase.reduce((acc, coinbase) => acc + coinbase.amount)
+
+    return this.coinbase
+  }
+
+  async collect (amount, splitBy = [1], addressType = 'legacy', fees = 0.005) {
+    // equal split may be specified by given desired number of UTXOs as an int
+    splitBy = typeof splitBy === 'object'
+      ? splitBy
+      : new Array(splitBy).fill(1 / splitBy)
+
+    // correct for floating point error
+    const correction = splitBy.reduce((acc, value) => acc + value) - 1
+    splitBy[0] -= correction
+
+    // collect coinbase transactions
+    await this.updateCoinbase()
+    amount = amount || this.coinbaseAmt
+
+    // generate rpc inputs to create transaction
+    const selectedInputs = selectTxInputs(this.coinbase, amount)
+
+    // generate transaction outputs
+    const transferAmounts = splitBy.map(portion => amount * portion)
+
+    const txOutputs = await pMap(
+      transferAmounts,
+      createOutput('random'),
+      { concurrency: 5 }
+    )
+
+    const changeAddress = await this.node.getNewAddress('change', addressType)
+    const rpcInput = rpcFormat(selectedInputs, txOutputs, changeAddress)
+
+    // create, sign and send tx
+    const txid = await this.send(...rpcInput)
+
+    return txid
+
+    // map transfer amount to format for rpc input
+    function createOutput (addressType) {
+      // randomise address type if desired
+      const addressFormat = addressType === 'random'
+        ? addressTypes[Math.floor(Math.random() * 3)]
+        : addressType
+
+      return async (amount) => {
+        const address = await node.getNewAddress('', addressFormat)
+
+        const output = {}
+
+        output[address] = castToValidBTCFloat(amount)
+        return output
+      }
+    }
+  }
+
+  async reorg (depth, height) {
+    assert(depth, 'reorg depth must be specified')
+    height = height || depth + 1
+
+    const currentHeight = await node.getblockcount()
+    const targetHash = await node.getblockhash(currentHeight - depth)
+    
+    await node.invalidateBlock(targetHash)
+    await node.generateToAddress(height)
+  }
+
+  async replaceByFee (inputs = [], outputs = [], flag = 'input') {
+    const mempool = this.node.getRawMempool()
+    const replaceTxns = []
+
+    // gather mempool transactions being replaced
+    await pMap(mempool, async txid => {
+      const tx = await this.node.getRawTransaction(txid, 1)
+      const repeatedInputs = tx.vin.filter(filterRepeats(inputs))
+      const repeatedOutputs = tx.vout.filter(filterRepeats(outputs))
+      if (repeatedInputs.length > 0 || repeatedOutputs.length > 0) {
+        tx.fees = feeCalculator(tx).absoluteFees
+        replaceTxns.push(tx)
+      }
+    }, { concurrency:  5 })
+
+    const replacedFees = replaceTxns.reduce((acc, tx) => acc + tx.fees, 0)
+    const replacedByteLength = replaceTxns.reduce((acc, tx) => acc + tx.hex.length / 2, 0)
+    const replaceFeeRate = replacedFees / replacedByteLength
+
+    const changeAddress = replaceTxns[0].vout.pop().addresses[0]
+
+    // in case fees are not set, calculate minimum fees required
+    if (!fees) fees = calculateRbfFee()
+
+    // construct actual rbfInput using
+    const rbfInput = rpcInput(inputs, outputs, changeAddress, fees) 
+    return this.send(...rbfInput)
+
+    // replace-by-fee helpers:
+    // filter for repeated utxos
+    function filterRepeats (txList) {
+      return object => {
+        return txList.indexOf(target => matchUtxo(target, object)) !== -1
+      }
+    }
+
+    // determine minimum fees required to replace tx
+    async function calculateRbfFee () {
+      const weightTestInput = rpcFormat(rbfInputs, rbfOutputs, changeAddress, 0.0005)
+      const weightTestRaw = await this.node.createRawTransaction(...weightTestInput)
+      const weightTestTx = await this.node.decodeRawTransaction(weightTestRaw)
+      
+      const weight = weightTestTx.weight
+      const minimumFeeByRate = weight * replaceFeeRate
+
+      const minimumFees = replacedFees > minimumFeeByRate ? replacedFees : minimumFeeByRate
+      return minimumFees + 0.000001
+    }
+  }
 }
 
+//  helper functions
+function matchUtxo (a, b) {
+  return a.txid === b.txid && a.vout === b.vout
+}
+
+// calculate fee details of a transaction
+function feeCalculator (tx) {
+  const inputAmount = tx.vin.reduce((acc, input) => acc + input.value, 0)
+  const outputAmount = tx.vout.reduce((acc, output) => acc + output.value, 0)
+
+  const absoluteFees = inputAmount - outputAmount
+
+  const byteLength = Buffer.from(tx.hex, 'hex').byteLength
+  const feeRate = absoluteFees / byteLength
+
+  return {
+    absoluteFee,
+    feeRate
+  }
+}
+
+// format transaction details to arguments to be passed to the rpc call
 function rpcFormat (inputs, outputs, changeAddress, fees = 0.0004) {
   const changeOutput = {}
- 
-  const inputTotal = inputs.reduce((acc, input) => acc +  input.amount, 0)
+
+  const inputTotal = inputs.reduce((acc, input) => acc + input.amount, 0)
   const outputTotal = outputs.reduce((acc, output) => acc + Object.values(output)[0], 0)
   console.log(outputTotal, inputs, inputTotal, fees)
 
   changeOutput[changeAddress] = castToValidBTCFloat(inputTotal - outputTotal - fees)
   console.log(changeOutput, 'changeOutput')
-  rpcOutputs = outputs.concat([changeOutput])
+  const rpcOutputs = outputs.concat([changeOutput])
 
-  const rpcInputs = inputs.map(input => { return {
-    txid: input.txid,
-    vout: input.vout
-  }})
+  const rpcInputs = inputs.map(input => {
+    return {
+      txid: input.txid,
+      vout: input.vout
+    }
+  })
 
   return [
     rpcInputs,
@@ -110,29 +242,14 @@ function rpcFormat (inputs, outputs, changeAddress, fees = 0.0004) {
   ]
 }
 
-function createOutput (addressType) {
-  addressFormat = addressType === 'random'
-    ? addressTypes[Math.floor(Math.random() * 3)]
-    : addressType
-
-  return async (amount) => {
-    const address = await node.getNewAddress('', addressFormat)
-
-    const output = {}
-
-    output[address] = castToValidBTCFloat(amount)
-    return output
-  }
-}
-
+// collect tx inputs
 function selectTxInputs (inputPool, amount) {
-  // assemble tx inputs
   const selectedInputs = []
   let selectedAmount = 0
 
   while (selectedAmount < amount) {
     const toTransfer = inputPool.pop()
-    
+
     selectedInputs.push(toTransfer)
 
     selectedAmount += toTransfer.amount
@@ -142,28 +259,10 @@ function selectTxInputs (inputPool, amount) {
   return selectedInputs
 }
 
-// build tx outputs
-function buildTxOutputs (transferAmounts, totalAmount, addresses) {
-  const txOutputs = []
-
-  for (let transferAmount of transferAmounts) {
-    const output = {}
-
-    const address = addresses.pop()
-
-    const fees = 0.005 * transferAmount / totalAmount
-
-    output[address] = castToValidBTCFloat(transferAmount - fees)
-    txOutputs.push(output)
-  }
-
-  return txOutputs
-}
-
-
-// update coinbaseTxns and return available coinbase funds 
+// update coinbaseTxns and return available coinbase funds
 async function coinbase (coinbaseTxns = []) {
   const unspent = await node.listUnspent()
+  console.log(unspent)
 
   const newUnspent = unspent.filter(utxo => !coinbaseTxns.includes(utxo))
 
@@ -171,7 +270,7 @@ async function coinbase (coinbaseTxns = []) {
 
   await pMap(newUnspent, async utxo => {
     const txInfo = await node.getTransaction(utxo.txid)
-    if (txInfo.generated) newCoinbaseTxns.push(utxo) 
+    if (txInfo.generated) newCoinbaseTxns.push(utxo)
   }, { concurrency: 5 })
 
   const returnArray = coinbaseTxns.concat(newCoinbaseTxns)
